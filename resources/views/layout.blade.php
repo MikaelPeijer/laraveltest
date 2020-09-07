d640a1aa85e283a3fdf81cfe29d4ecf5333ecaf2
Mikael Peijer <9006825@student.zadkine.nl>
2020-09-07 16:08:11 +0200

    html changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,34 +1,16 @@
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by TEMPLATED
-http://templated.co
-Released for free under the Creative Commons Attribution License
-
-Name       : SimpleWork 
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20140225
-
--->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link href="{{ asset('css/default.css') }}" rel="stylesheet">
-<link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
-
-@yield ('head')
-
-
-<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
-
-</body>
-
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+	<link href="{{ asset('css/default.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
+	@yield ('head')
+</head>
+<body>
 <div id="header-wrapper">
 	<div id="header" class="container">
 		<div id="logo">
@@ -45,10 +27,9 @@ Released   : 20140225
 			</ul>
         </div>
         
-        
     </div>
     @yield ('header')
  
-
-@yield ('content')
+	@yield ('content')
+</body>
 </html>
